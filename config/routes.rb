74288e735a872504fdca6cf87357c3a951eb9b80
Tom Marrs <thomasamarrs@comcast.net>
2014-08-13 14:48:29 -0600

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 
   # Example of regular route:
